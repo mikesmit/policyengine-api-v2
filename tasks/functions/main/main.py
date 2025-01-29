78382ba2e249fa36f26900d5d88a1a533bae75d9
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-01-29 18:19:11 +1100

    Add print of args

--- tasks/functions/main/main.py
+++ tasks/functions/main/main.py
@@ -31,6 +31,7 @@ def execute_job_from_id(job_id: int):
 @functions_framework.http
 def main(request: flask.Request) -> dict:
     # Get job_id from query parameter
+    print(request.args)
     job_id = int(request.args.get("job_id"))
 
     execute_job_from_id(job_id)
