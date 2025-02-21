46d41936927481013db6d09ddd5075aa7af7a87d
Anthony Volk <anth.volk@gmail.com>
2025-02-21 01:19:38 +0100

    fix: Correct declaration of CountryId

--- libs/policyengine-api/src/policyengine_api/api/models/country_id.py
+++ libs/policyengine-api/src/policyengine_api/api/models/country_id.py
@@ -1,11 +1,13 @@
-from pydantic import validator
+from pydantic import BaseModel, field_validator
 from policyengine_api.api.utils.constants import COUNTRIES
 
 
-class CountryId(str):
+class CountryId(BaseModel):
+    country_id: str
 
-    @validator("country_id")
-    def validate_country_id(cls, v):
-        if v not in COUNTRIES:
+    @field_validator("country_id", mode="after")
+    @classmethod
+    def validate_country_id(cls, country: str) -> str:
+        if country not in COUNTRIES:
             raise ValueError("Invalid country ID")
-        return v
+        return country
