4003f7f28207701b5c5ef909dbc1a767329d4aaf
SakshiKekre <sakshi.kekre@gmail.com>
2025-05-27 13:58:57 -0700

    Retaining comment for country option

--- projects/policyengine-apis-integ/tests/simulation/test_calculate.py
+++ projects/policyengine-apis-integ/tests/simulation/test_calculate.py
@@ -4,15 +4,14 @@ import pytest
 @pytest.mark.asyncio
 async def test_calculation(async_client):
     options = policyengine_simulation_api_client.SimulationOptions(
-        country="us",
-        scope="macro",
-        reform={
-            "gov.irs.credits.ctc.refundable.fully_refundable": 
-                policyengine_simulation_api_client.ParametricReformValue.from_dict({"2023-01-01.2100-12-31":True})
-        },
-        subsample=200, # reduce the number of households to speed things up.
-        data=policyengine_simulation_api_client.Data("gs://policyengine-us-data/cps_2023.h5")
-    )
+            country="us", # don't use uk. It will try to load extra stuff from huggingface
+            scope="macro",
+            reform={
+                "gov.irs.credits.ctc.refundable.fully_refundable": policyengine_simulation_api_client.ParametricReformValue.from_dict({"2023-01-01.2100-12-31":True})
+            },
+            subsample=200, # reduce the number of households to speed things up.
+            data=policyengine_simulation_api_client.Data("gs://policyengine-us-data/cps_2023.h5") # force the service to use google storage (policyengine.py defaults to huggingface)
+        )
     response = await async_client.call(
         "simulate_simulate_economy_comparison_post",
         options
