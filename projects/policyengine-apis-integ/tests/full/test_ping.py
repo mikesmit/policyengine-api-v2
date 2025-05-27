4b4e91ec4434c8bf417b69236c5892c6e6c79b72
SakshiKekre <sakshi.kekre@gmail.com>
2025-05-27 13:32:50 -0700

    Modifed tests to enable async execution using wrapper over client

--- projects/policyengine-apis-integ/tests/full/test_ping.py
+++ projects/policyengine-apis-integ/tests/full/test_ping.py
@@ -1,36 +1,14 @@
 import policyengine_full_api_client
-from pydantic_settings import BaseSettings, SettingsConfigDict
 import pytest
-
-
-class Settings(BaseSettings):
-    base_url: str = "http://localhost:8000"
-    access_token: str | None = None
-    timeout_in_millis: int = 200
-
-    model_config = SettingsConfigDict(env_prefix="full_integ_test_")
-
-
-settings = Settings()
-
-
-@pytest.fixture()
-def client() -> policyengine_full_api_client.DefaultApi:
-    config = policyengine_full_api_client.Configuration(host=settings.base_url)
-    client = policyengine_full_api_client.ApiClient(config)
-    if settings.access_token:
-        client.default_headers["Authorization"] = (
-            f"Bearer {settings.access_token}"
-        )
-    return policyengine_full_api_client.DefaultApi(client)
-
 
 # I don't love this client. We should investigate alternatives.
 # the package structure is really tedious to use
 # it doesn't define methods on the client so it's not very OO
 # and it returns a union instead of just throwing an exception
-def test_ping(client: policyengine_full_api_client.DefaultApi):
-    response = client.ping_ping_post(
+@pytest.mark.asyncio
+async def test_ping(async_client):
+    response = await async_client.call(
+        "ping_ping_post",
         policyengine_full_api_client.PingRequest(value=12)
     )
     assert response.incremented == 13
--- projects/policyengine-apis-integ/tests/simulation/test_calculate.py
+++ projects/policyengine-apis-integ/tests/simulation/test_calculate.py
@@ -1,16 +1,20 @@
 import policyengine_simulation_api_client
 import pytest
 
-def test_calculation(client: policyengine_simulation_api_client.DefaultApi):
+@pytest.mark.asyncio
+async def test_calculation(async_client):
     options = policyengine_simulation_api_client.SimulationOptions(
-            country="us", # don't use uk. It will try to load extra stuff from huggingface
-            scope="macro",
-            reform={
-                "gov.irs.credits.ctc.refundable.fully_refundable": policyengine_simulation_api_client.ParametricReformValue.from_dict({"2023-01-01.2100-12-31":True})
-            },
-            subsample=200, # reduce the number of households to speed things up.
-            data=policyengine_simulation_api_client.Data("gs://policyengine-us-data/cps_2023.h5") # force the service to use google storage (policyengine.py defaults to huggingface)
-        )
-    response = client.simulate_simulate_economy_comparison_post(
+        country="us",
+        scope="macro",
+        reform={
+            "gov.irs.credits.ctc.refundable.fully_refundable": 
+                policyengine_simulation_api_client.ParametricReformValue.from_dict({"2023-01-01.2100-12-31":True})
+        },
+        subsample=200, # reduce the number of households to speed things up.
+        data=policyengine_simulation_api_client.Data("gs://policyengine-us-data/cps_2023.h5")
+    )
+    response = await async_client.call(
+        "simulate_simulate_economy_comparison_post",
         options
     )
+    assert response is not None
