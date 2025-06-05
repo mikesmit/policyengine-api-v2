21cddd1c996a498d0a111d5dc49153ff0b5e443b
Michael Smit <mikesmit@coderberry.com>
2025-06-05 11:05:30 -0700

    Corretly capture the current revision in metadata.
    Fixes #237
    
    This fixes an issue where we were recording metadata associated with the previous revision of the simulation api
    instead of the one we just deployed.
    
    This was happening because the cloud_run_api_v2 module will resolve the currently live revision name instead of the one it's deployed.
    
    Change to explicitly defining the new revision name and then referencing that instead.

--- terraform/infra-policyengine-api/modules/fastapi_cloudrun/outputs.tf
+++ terraform/infra-policyengine-api/modules/fastapi_cloudrun/outputs.tf
@@ -2,8 +2,8 @@ output "uri" {
     value = google_cloud_run_v2_service.api.uri
 }
 
-output "latest_ready_revision" {
-    value = google_cloud_run_v2_service.api.latest_ready_revision
+output "revision" {
+    value = local.revision_name
 }
 
 output "location" {
