091f6a08c02d1ccfb45bd801b4a0304551972b95
Michael Smit <mikesmit@coderberry.com>
2025-06-11 09:27:16 -0700

    Migrate integ tests to uv

--- common.mk
+++ common.mk
@@ -6,11 +6,26 @@ remove_artifacts:
 install:
 	uv sync --extra test --extra build
 
-format:
-	uv run black .
-
 checkformat:
-	uv run black --check src
+	@dirs=""; \
+	[ -d "src" ] && dirs="$$dirs src"; \
+	[ -d "tests" ] && dirs="$$dirs tests"; \
+	if [ -n "$$dirs" ]; then \
+		uv run black --check $$dirs; \
+	else \
+		echo "Neither 'src' nor 'tests' directory found. Skipping Black formatting."; \
+	fi
+
+format:
+	@dirs=""; \
+	[ -d "src" ] && dirs="$$dirs src"; \
+	[ -d "tests" ] && dirs="$$dirs tests"; \
+	if [ -n "$$dirs" ]; then \
+		echo "Running Black on:$$dirs"; \
+		uv run black $$dirs; \
+	else \
+		echo "Neither 'src' nor 'tests' directory found. Skipping Black formatting."; \
+	fi
 
 pyright:
 	uv run pyright 
