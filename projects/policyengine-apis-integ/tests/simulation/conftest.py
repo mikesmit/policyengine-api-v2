6c8b8c52633e7fefce3579dc6b8e3817f981fe7c
Michael Smit <mikesmit@coderberry.com>
2025-06-11 09:27:30 -0700

    Apply formatting to integ tests

--- projects/policyengine-apis-integ/tests/simulation/conftest.py
+++ projects/policyengine-apis-integ/tests/simulation/conftest.py
@@ -2,6 +2,7 @@ import policyengine_simulation_api_client
 import pytest
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     base_url: str = "http://localhost:8001"
     access_token: str | None = None
@@ -12,12 +13,11 @@ class Settings(BaseSettings):
 
 settings = Settings()
 
+
 @pytest.fixture()
 def client() -> policyengine_simulation_api_client.DefaultApi:
     config = policyengine_simulation_api_client.Configuration(host=settings.base_url)
     client = policyengine_simulation_api_client.ApiClient(config)
     if settings.access_token:
-        client.default_headers["Authorization"] = (
-            f"Bearer {settings.access_token}"
-        )
+        client.default_headers["Authorization"] = f"Bearer {settings.access_token}"
     return policyengine_simulation_api_client.DefaultApi(client)
--- projects/policyengine-apis-integ/tests/simulation/test_calculate_cliffs.py
+++ projects/policyengine-apis-integ/tests/simulation/test_calculate_cliffs.py
@@ -1,24 +1,25 @@
 import policyengine_simulation_api_client
 
+
 def test_calculation_cliffs(client: policyengine_simulation_api_client.DefaultApi):
     options = policyengine_simulation_api_client.SimulationOptions(
-            country="us", # don't use uk. It will try to load extra stuff from huggingface
-            scope="macro",
-            reform = {
-                "gov.irs.credits.eitc.max[0].amount": policyengine_simulation_api_client.ParametricReformValue.from_dict({
-                    "2026-01-01.2100-12-31": 0
-                })
-            },
-            include_cliffs=True,
-            subsample=200, # reduce the number of households to speed things up.
-            data=policyengine_simulation_api_client.Data("gs://policyengine-us-data/cps_2023.h5") # force the service to use google storage
-        )
-    response = client.simulate_simulate_economy_comparison_post(
-        options
+        country="us",  # don't use uk. It will try to load extra stuff from huggingface
+        scope="macro",
+        reform={
+            "gov.irs.credits.eitc.max[0].amount": policyengine_simulation_api_client.ParametricReformValue.from_dict(
+                {"2026-01-01.2100-12-31": 0}
+            )
+        },
+        include_cliffs=True,
+        subsample=200,  # reduce the number of households to speed things up.
+        data=policyengine_simulation_api_client.Data(
+            "gs://policyengine-us-data/cps_2023.h5"
+        ),  # force the service to use google storage
     )
+    response = client.simulate_simulate_economy_comparison_post(options)
     result = response.to_dict()
     # Check that cliff impact data is present in the simulation result
-    cliffs = result.get('cliff_impact')
+    cliffs = result.get("cliff_impact")
     assert cliffs is not None, "Expected 'cliff_impact' to be present in the output."
 
     # Assert that cliff impact is non-zero in both baseline and reform scenarios
