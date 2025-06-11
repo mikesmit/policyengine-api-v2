da1c698b4d758f7060f591df5af451131b0629d5
Michael Smit <mikesmit@coderberry.com>
2025-06-11 07:04:40 -0700

    Added support for a generate target
    And remove the artifact directory as part of build...

--- common.mk
+++ common.mk
@@ -1,4 +1,7 @@
-build: install checkformat pyright test
+build: remove_artifacts install checkformat pyright generate test 
+
+remove_artifacts:
+	rm -rf artifacts
 
 install:
 	uv sync --extra test --extra build
@@ -14,3 +17,6 @@ pyright:
 
 test:
 	uv run pytest
+
+generate:
+	echo "No code generation target defined"
